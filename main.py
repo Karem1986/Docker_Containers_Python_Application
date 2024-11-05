473b460bbe5137301dd66772844911763a381466
Karin Ortiz <karin.ortiz@movares.nl>
2024-11-05 13:54:52 +0100

    corrected typos

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ print(which_pie)
 
 if which_pie == 'apple':
   print('Your pie will have as ingredients: ')
-  apple_pie = ['flour', 'soya drink', 'sugar', 'cinammon']
+  apple_pie = ['flour', 'soya drink', 'sugar', 'cinnamon']
   print(apple_pie)
-elif which_pie == 'leek potatoes':
+elif which_pie == 'leek potato':
  leek_potato_pie = ['leeks', 'soya drink', 'onions', 'potatoes']
  print('Your pie will have as ingredients: ')
  print(list(leek_potato_pie))
